c0aae3bc525432ee65c3e4c32a8b2cfdf8843f8f
Robbie Jaeger <jaeger.rob@gmail.com>
2022-01-10 20:22:11 -0700

    Render campsite with marker and popup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Map from './Map';
+import { Campsite } from './Models';
 
 function App() {
-
+  const [campsites] = useState<Array<Campsite>>([{title: "Big Pool", location: {lat: 40.2744767, long: -105.7643305}, datesAvailable: [], elevation: "9,160 ft", detailsLink: "https://www.nps.gov/romo/planyourvisit/upload/072-Big-Pool-2017-2.pdf"}]);
 
   return (
     <main className="App">
       <h1>RMNP Campsite Planner</h1>
 
-      <Map />
+      <Map campsites={campsites}/>
     </main>
   );
 }
--- src/Map.tsx
+++ src/Map.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Campsite } from './Models';
 
-function Map() {
+type MapProps = {
+  campsites: Campsite[]
+}
+
+function Map({ campsites }:MapProps) {
+  const campsiteMarkers = campsites.map(campsite => {
+    return (
+      <Marker position={[campsite.location.lat, campsite.location.long]}>
+        <Popup>
+          <h3>{campsite.title}</h3>
+          <a href={campsite.detailsLink} target="_blank" rel="noreferrer">Site Details</a>
+        </Popup>
+      </Marker>
+    )
+  });
+
   return (
     <MapContainer className="MapContainer" center={[40.3428, -105.6836]} zoom={11}>
       <TileLayer
         attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
         url="https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png"
       />
+      {campsiteMarkers}
     </MapContainer>
   );
 }
