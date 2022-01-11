d0fb087c092dd2609957e7b8ad37e411ab505210
Robbie Jaeger <jaeger.rob@gmail.com>
2022-01-10 20:31:12 -0700

    Add multiple campsites loaded with useEffect to simluate netowrk request flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Map from './Map';
 import { Campsite } from './Models';
 
 function App() {
-  const [campsites] = useState<Array<Campsite>>([{title: "Big Pool", location: {lat: 40.2744767, long: -105.7643305}, datesAvailable: [], elevation: "9,160 ft", detailsLink: "https://www.nps.gov/romo/planyourvisit/upload/072-Big-Pool-2017-2.pdf"}]);
+  const [campsites, setCampsites] = useState<Array<Campsite>>([]);
+
+  useEffect(() => {
+    setCampsites([
+      {title: "Big Pool", location: {lat: 40.2744767, long: -105.7643305}, datesAvailable: [], elevation: "9,160 ft", detailsLink: "https://www.nps.gov/romo/planyourvisit/upload/072-Big-Pool-2017-2.pdf"},
+      {title: "North Inlet Falls", location: {lat: 40.269919, long: -105.7248948}, datesAvailable: [], elevation: "9,540 ft", detailsLink: "https://www.nps.gov/romo/planyourvisit/upload/079-North-Inlet-Falls-2021-1.pdf"}
+    ])
+  }, []);
 
   return (
     <main className="App">
