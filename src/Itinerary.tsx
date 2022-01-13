2a1d246995a2359133815247ce9eb3c0ec1674f2
Robbie Jaeger <jaeger.rob@gmail.com>
2022-01-12 20:46:13 -0700

    Add booking cards

--- src/Itinerary.tsx
+++ src/Itinerary.tsx
@@ -10,6 +10,10 @@ import DateAdapter from '@mui/lab/AdapterDayjs';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import Button from '@mui/material/Button';
 
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import { CardActionArea } from '@mui/material';
+
 function Itinerary() {
   const [firstNight, setFirstNight] = useState<null | Dayjs>(null);
   const [lastNight, setLastNight] = useState<null | Dayjs>(null);
@@ -17,9 +21,7 @@ function Itinerary() {
   return (
     <Paper sx={{p: 3}} elevation={2}>
       <Stack spacing={2}>
-        <Typography variant="h4">
-          Itinerary
-        </Typography>
+        <Typography variant="h4">Itinerary</Typography>
 
         <LocalizationProvider dateAdapter={DateAdapter}>
           <DatePicker
@@ -44,6 +46,37 @@ function Itinerary() {
         </LocalizationProvider>
 
         <Button variant="contained">See Availability</Button>
+      </Stack>
+
+      <Stack sx={{mt: 4}} spacing={2}>
+        <Typography variant="h4">Campsite Bookings</Typography>
+
+        <Card elevation={4}>
+          <CardActionArea>
+            <CardContent>
+              <Typography variant="h6">08/13/2022</Typography>
+              <Typography variant="body1">Click to see campsite availability</Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+
+        <Card elevation={4}>
+          <CardActionArea>
+            <CardContent>
+              <Typography variant="h6">08/14/2022</Typography>
+              <Typography variant="body1">Big Pool</Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+
+        <Card elevation={4}>
+          <CardActionArea>
+            <CardContent>
+              <Typography variant="h6">08/15/2022</Typography>
+              <Typography variant="body1">Select campsite on map</Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
       </Stack>
     </Paper>
   )
